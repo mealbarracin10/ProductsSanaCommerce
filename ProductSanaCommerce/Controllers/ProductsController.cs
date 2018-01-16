117f0361657484263d6ed08efa8ae33520a880f3
Martin  Albarracin <edgar3280@gmail.com>
2018-01-15 22:15:08 -0500

    continue integration for app Sana commerce

---
+++ ProductSanaCommerce/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using ProductSanaCommerce.Models;
+
+namespace ProductSanaCommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public string pathFile = WebConfigurationManager.AppSettings["pathFile"];
+        public string nameFile = WebConfigurationManager.AppSettings["NameFile"];
+        public string extensionFile = WebConfigurationManager.AppSettings["ExtensionFile"];
+        public string DELIMITER = ",";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product products)
+        {
+            string NameOfFile = pathFile + nameFile + extensionFile;
+            StringBuilder builder = new StringBuilder();
+            builder.Append(products.Name).Append(DELIMITER);
+            builder.Append(products.Price).Append(DELIMITER);
+            builder.Append(products.Quantity).Append(DELIMITER);
+            builder.Append(products.Description).Append(DELIMITER); ;
+            System.IO.File.WriteAllText(NameOfFile, builder.ToString());
+            return this.Json(new { EnableSuccess = true, SuccessTitle = "Success", SuccessMsg = "awesome" });
+        }
+    }
+}
---
+++ ProductSanaCommerce/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductSanaCommerce.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public  int Quantity{get;set;}
+
+        public string Description{ get; set;}
+    }
+}
